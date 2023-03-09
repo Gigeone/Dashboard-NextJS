e7b5e7366ea689888f3c29ccd86a0921684ebc4e
Gigeone <matthieugh@gmail.com>
2023-03-09 16:17:22 +0100

    deploy

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,10 @@
+import SideBar from '@/Components/SideBar'
 import '@/styles/globals.css'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <SideBar>
+      <Component {...pageProps} />
+    </SideBar>
+  )
 }
